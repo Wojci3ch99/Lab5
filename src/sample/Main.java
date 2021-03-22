c1acb15bbfd9b04e39f98052f7b1b6db07c7e48d
Wojci3ch99 <Wojciech9905@gmail.com>
2021-03-22 13:18:48 +0100

    Skończone Zadanie 3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,8 +11,11 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+
 
 import java.awt.*;
+import java.sql.Time;
 
 public class Main extends Application {
 
@@ -30,15 +35,31 @@ public class Main extends Application {
     public void start(Stage stage){
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Timeline t = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
+        t.setCycleCount(Timeline.INDEFINITE);
+
         stage.setTitle("Kulki!");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
+//        gc.setFill(Color.BLACK);
+//        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
+//
+//        gc.setFill(Color.WHITESMOKE);
+//        gc.fillOval(ARENAX1+ARENAWIDTH/2, ARENAY1+ARENAHEIGHT/2, 2*R, 2*R);
+        t.play();
+
+    }
+
+    private double x = ARENAX1+ARENAWIDTH/2;
+
+    private void run(GraphicsContext gc) {
         gc.setFill(Color.BLACK);
         gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
 
         gc.setFill(Color.WHITESMOKE);
-        gc.fillOval(ARENAX1+ARENAWIDTH/2, ARENAY1+ARENAHEIGHT/2, 2*R, 2*R);
+        gc.fillOval(x += 5, ARENAY1+ARENAHEIGHT/2, 2*R, 2*R);
     }
 
 
